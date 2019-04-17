fd8e15bc9688acf9a1a5768e7a2524cc6dec7e60
Abhishek Ranjan <aranjan@nymi.com>
2019-04-17 16:10:56 -0400

    Updated some viz and graphs

--- code/vsb_viz.py
+++ code/vsb_viz.py
@@ -3,6 +3,7 @@ import numpy as np
 import feautils
 import xgboost as xgb 
 
+bShow = False
 settings = feautils.default_settings()
 
 model = np.load(settings['SAVED_MODEL']).ravel()[0]
@@ -12,19 +13,22 @@ train_fea = np.load(settings['TRAIN_FEATURES'])
 reg = train_fea[0,:]
 fault = train_fea[3,:]
 X = np.arange(0, reg.shape[0])
+plt.figure()
 plt.scatter(X, reg, color='b', marker='x', label='Normal')
 plt.scatter(X, fault, color='r', marker='x', label='Fault')
 plt.grid()
 plt.legend()
 plt.title('LBP feature scatter plot')
-plt.show()
+plt.savefig(fname='../docs/lbp_scatter.svg', format='svg')
+if bShow:
+    plt.show()
 
 ## ANALYZE TOP FEATURES
 all_fscores = model.get_fscore()
 fea_names = list(all_fscores.keys())
 print('Total features used by the model = {}'.format(len(fea_names)))
 
-TOP_FEA = 10
+TOP_FEA = 20
 FEA_DESC = []
 print('Feature descriptions for {} top features'.format(TOP_FEA))
 f_vals = list(all_fscores.values())
@@ -34,5 +38,20 @@ for i in np.arange(1, TOP_FEA+1):
     fea_ind = int(f_keys[sorted_ind[-1*i]][1:])
     FEA_DESC.append(feautils.fea_desc(fea_ind))
 
+plt.figure()
 xgb.plot_importance(booster=model, max_num_features=TOP_FEA)
-plt.show()
+plt.savefig(fname='../docs/fea_importance.svg', format='svg')
+if bShow:
+    plt.show()
+
+# Show feature weight distribution
+wt = np.asarray(f_vals)
+wt_norm = np.divide(wt, np.max(wt))
+log_wts = np.log10(wt_norm) 
+plt.figure()
+plt.hist(log_wts, bins=100)
+plt.grid()
+plt.title('Histogram of log10 of weights in range [{:.2f}, {:.2f}]'.format(np.min(log_wts), np.max(log_wts)))
+plt.savefig(fname='../docs/wt_hist.svg', format='svg')
+if bShow:
+    plt.show()
